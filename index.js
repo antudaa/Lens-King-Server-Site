9389d803a0521e52ca0e09bae0e0c30dae9c0158
Antu_Das <antuchandradasp9@gmail.com>
2022-11-09 07:28:22 -0800

    Added more data

--- index.js
+++ index.js
@@ -16,8 +16,33 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p4iytrv.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollections = client.db('lensKing').collection('services');
+
+        app.get('/homeServices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollections.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollections.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err.message));
 
 
 
