0e64d83455d6f0d7005db351936659717c355c60
Antu_Das <antuchandradasp9@gmail.com>
2022-11-10 05:58:24 -0800

    Added service post functionality

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ async function run(){
             res.send(services);
         });
 
+        app.post('/services', async (req, res) => {
+            const review = req.body;
+            const result = await serviceCollections.insertOne(review);
+            res.send(result);
+        });
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id : ObjectId(id)};
